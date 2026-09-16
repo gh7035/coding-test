770b09174b243b24507709e6397e707c0600abaa
gh7035 <146627024+gh7035@users.noreply.github.com>
2026-09-16 19:02:17 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    
+    answer = 0
+    
+#     num1, a, num2 = my_string.split()
+    
+    
+#     if a == "+":
+#         answer = int(num1)+int(num2)
+#     else:
+#         answer = int(num1) - int(num2)
+
+    answer = eval(my_string)
+    return answer
